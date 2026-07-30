c6310b112b521827f481d3f94ff8467071e298fd
louis1618 <louis.devop@gmail.com>
2026-07-31 01:33:55 +0900

    feat: initialize Once UI configuration with custom design tokens and routes

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://moring.co";
+const baseURL: string = "https://luverse.moring.co";
 
 const routes: RoutesConfig = {
   "/": true,
